9cd88701e6ea9a76e16cd382e859a2f54dc99d61
Daniel <daniwar@gmail.com>
2019-02-20 13:13:58 +0200

    Initial Commit

---
+++ Sbor na 2 chisla/Sbor na 2 chisla/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sbor_na_2_chisla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
